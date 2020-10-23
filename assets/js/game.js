d141a86dbd5598f760094ba43ec69e68f5d65fdf
SleepingNext <veroskevin8@gmail.com>
2020-10-23 21:27:11 +0700

    Initial Commit

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+function runAway() {
+  var screenWidth = screen.width;
+  var screenHeight = screen.height;
+
+  var newX = Math.floor(Math.random() * (screenWidth - 200)) + 1;
+  var newY = Math.floor(Math.random() * (screenHeight - 250)) + 1;
+
+  document.getElementById("game-wrapper").style.transform =
+    "translate(" + newX + "px," + newY + "px)";
+}
---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+	<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+	<!-- <link rel="manifest" href="/site.webmanifest"> -->
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
+	<title>SleepingNext</title>
+	<link href="assets/css/style.css" rel="stylesheet">
+	<script src="assets/js/game.js"></script>
+</head>
+
+<body>
+	<div id="welcome-wrapper" class="centered">
+		<img id="ithoughts" src="assets/img/ithoughts-2.gif">
+		<h1>Please help SleepingNext capturing the idea.</h1>
+	</div>
+	<div onclick="runAway()" onmouseover="runAway()" id="game-wrapper">
+		<img id="idea" src="assets/img/idea.png">
+		<p>Capture The Idea!</p>
+	</div>
+	<!-- <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> -->
+	<!-- light bulb from: https://www.stockvault.net/photo/174577/light-bulb-ideas -->
+	<!-- ithoughts from: https://dribbble.com/shots/3681584-/attachments/3681584-?mode=media -->
+</body>
+
+</html>
